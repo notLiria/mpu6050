7be84ab68d927afc1f617b0369d014206d781722
Liria <thisisntarealemail@nowhere.com>
2019-12-13 04:26:37 +0000

    Added some I2C communication methods

--- mpu6050/mpu6050.py
+++ mpu6050/mpu6050.py
@@ -3,7 +3,56 @@ between a Raspberry Pi and a MPU-6050 Gyroscope / Accelerometer combo.
 Made by: MrTijn/Tijndagamer
 Released under the MIT License
 Copyright (c) 2015, 2016, 2017 MrTijn/Tijndagamer
+
+CHANGES 2019/11/16
+- Added documentation
+- Added read_i2c_byte(self, register)
+- Added read_i2c_bit(self, register, bitNum)
+- Added read_i2c_bits(self, register, bitStart, length)
+- Added get_accel_x(self)
+- Added get_accel_y(self)
+- Added get_accel_z(self)
+- Added get_gyro_x(self)
+- Added get_gyro_y(self)
+- Added get_gyro_z(self)
+- Added reset(self)
 """
+
+
+
+"""
+Method List
+read_i2c_byte(self, register): Reads a byte from register
+read_i2c_bit(self, register, bitNum): Reads bitNum from register
+read_i2c_bits(self, register, bitStart, length): Reads length bits from register, starting at bitNum
+read_i2c_word(self, register): Reads a byte from register
+write_i2c_bit(self, register, bitNum, value): Writes value to bitNum on register
+write_i2c_bits(self, register, bitStart, length): Writes length bits to bitNum on register
+get_temp(self): Reads the temperature from onboard temperature sensor
+set_accel_range(self, accel_range): Sets the accelerometer to range
+read_accel_range(self, raw): Reads the range the accelerometer is set to
+get_accel_data(self): Returns the x, y, z values from the accelerometer, scaled
+get_accel_x(self): Returns the raw x value from the accelerometer 
+get_accel_y(self): Returns the raw y value from the accelerometer 
+get_accel_z(self): Returns the raw z value from the accelerometer 
+set_gyro_range(self, gyro_range): Sets gyroscope range to gyro_range
+read_gyro_range(self, raw): Reads the range the gyroscope is set to
+get_gyro_data(self): Gets and returns x, y, and z values from the gyroscope
+get_gyro_x(self): Returns the raw x value from the gyro
+get_gyro_y(self): Returns the raw y value from the gyro
+get_gyro_z(self): Returns the raw z value from the gyro
+get_all_data(self): Reads and returns all available data
+reset(self): Resets the device
+get_sleep_enabled(self): Checks if sleep is enabled
+set_sleep_enabled(self, enabled): Set sleep mode status
+get_wake_cycle_enabled(self): Get wake cycle enabled status
+set_wake_cycle_enabled(self, enabled): set wake cycle enabled status
+get_temp_sensor_enabled(self): Get temp sensor enabled status
+set_temp_sensor_enabled(self, enabled): Set temp senosr enabled status
+get_clock_source(self): Gets the clock source
+"""
+
+
 
 import smbus
 
@@ -40,6 +89,23 @@ class mpu6050:
     PWR_MGMT_1 = 0x6B
     PWR_MGMT_2 = 0x6C
 
+    #Added 2019/11/16
+    PWR1_DEVICE_RESET_BIT = 7
+    PWR1_DEVICE_SLEEP_BIT = 6
+    PWR1_CYCLE_BIT = 5
+    PWR1_TEMP_DIS_BIT = 3
+    PWR1_CLKSEL_BIT = 2
+    PWR1_CLKSEL_LENGTH = 3
+
+    PWR2_LP_WAKE_CTRL_BIT = 7
+    PWR2_LP_WAKE_CTRL_LENGTH = 2
+    PWR2_STBY_XA_BIT = 5
+    PWR2_STBY_YA_BIT = 4
+    PWR2_STBY_XG_BIT = 3
+    PWR2_STBY_YG_BIT = 1
+    PWR2_STBY_ZG_BIT = 0
+    
+
     ACCEL_XOUT0 = 0x3B
     ACCEL_YOUT0 = 0x3D
     ACCEL_ZOUT0 = 0x3F
@@ -61,6 +127,40 @@ class mpu6050:
 
     # I2C communication methods
 
+
+    def read_i2c_byte(self, register):
+        """
+        Read one byte from the specified register
+        Inputs: register -- the register to read
+        Outputs: The read result
+        """
+        return self.bus.read_byte_data(self.address, register)
+   
+    def read_i2c_bit(self, register, bitNum):
+        """
+        Reads the bitNum-th bit from register
+        Inputs: register - the register to read
+                bitNum - the bit to read
+        OutputS: The bit at bitNum 
+        """
+        data = self.read_i2c_byte(register)
+        return data &  (1 << bitNum)
+
+    def read_i2c_bits(self, register, bitStart, length):
+        """
+        Read multiple bits from an 8-bit device register.
+        Inputs: register - the register to read
+                bitStart - First bit position to read (0-7)
+                length - number of bits to read (no more than 8)
+        """
+        data = self.read_i2c_byte(register)
+        if (data != 0):
+            mask = ((1 << length) - 1) << (bitStart - length + 1)
+            data &= mask
+            data >>= (bitStart - length + 1)
+            return data
+        
+        
     def read_i2c_word(self, register):
         """Read two i2c registers and combine them.
 
@@ -77,6 +177,26 @@ class mpu6050:
             return -((65535 - value) + 1)
         else:
             return value
+
+    def write_i2c_bit(self, register, bitNum, value):
+        data = self.read_i2c_byte(register)
+        if (data != 0):
+            data = (data | (1 << bitNum))
+        else:
+            data = (data & ~(1 << bitNum))
+        self.bus.write_byte_data(self.address, register, data)
+        
+
+    def write_i2c_bits(self, register, bitStart, length, data):
+        b = self.read_i2c_byte(self.address)
+        if (data != 0):
+            mask = ((1 << length) - 1) << (bitStart - length + 1)
+            data <<= (bitStart - length + 1)
+            data &= mask
+            b &= ~(mask)
+            b |= data
+            return self.bus.write_byte_data(self.address, register, b)
+            
 
     # MPU-6050 Methods
 
@@ -130,7 +250,7 @@ class mpu6050:
                 return -1
 
     def get_accel_data(self, g = False):
-        """Gets and returns the X, Y and Z values from the accelerometer.
+        """Gets and returns the X, Y and Z values from the accelerometer., scaled
 
         If g is True, it will return the data in g
         If g is False, it will return the data in m/s^2
@@ -166,7 +286,29 @@ class mpu6050:
             y = y * self.GRAVITIY_MS2
             z = z * self.GRAVITIY_MS2
             return {'x': x, 'y': y, 'z': z}
+        
+    def get_accel_x(self):
+        """ 
+        Gets and returns the raw X value from the acceloremetr
+        Returns x-axis acceleration measurement in 16 bit
+        """
+        return self.read_i2c_word(self.ACCEL_XOUT0)
 
+    def get_accel_y(self):
+        """ 
+        Gets and returns the raw y value from the acceloremetr
+        Returns y-axis acceleration measurement in 16 bit 
+        """
+        return self.read_i2c_word(self.ACCEL_YOUT0)
+
+    def get_accel_z(self):
+        """ 
+        Gets and returns the raw z value from the acceloremetr
+        Returns z-axis acceleration measurement in 16 bit 
+        """
+        return self.read_i2c_word(self.ACCEL_ZOUT0)
+
+        
     def set_gyro_range(self, gyro_range):
         """Sets the range of the gyroscope to range.
 
@@ -186,7 +328,7 @@ class mpu6050:
         register.
         If raw is False, it will return 250, 500, 1000, 2000 or -1. If the
         returned value is equal to -1 something went wrong.
-        """
+o        """
         raw_data = self.bus.read_byte_data(self.address, self.GYRO_CONFIG)
 
         if raw is True:
@@ -233,6 +375,27 @@ class mpu6050:
 
         return {'x': x, 'y': y, 'z': z}
 
+    def get_gyro_x(self):
+        """
+        Gets and returns the raw x value from the gyroscope
+        Returns x-axis acceleration measurement in 16 bit 
+        """
+        return self.read_i2c_word(self.GYRO_XOUT0)
+
+    def get_gyro_y(self):
+        """
+        Gets and returns the raw y value from the gyroscope
+        Returns y-axis acceleration measurement in 16 bit 
+        """
+        return self.read_i2c_word(self.GYRO_YOUT0)
+    
+    def get_gyro_z(self):
+        """
+        Gets and returns the raw z value from the gyroscope
+        Returns z-axis acceleration measurement in 16 bit 
+        """
+        return self.read_i2c_word(self.GYRO_ZOUT0)
+
     def get_all_data(self):
         """Reads and returns all the available data."""
         temp = self.get_temp()
@@ -240,6 +403,69 @@ class mpu6050:
         gyro = self.get_gyro_data()
 
         return [accel, gyro, temp]
+
+    #PWR_MGMT_1 Register
+    def reset(self):
+        """
+        Triggers a device reset. A small delay of ~50ms may be desirable after triggering a reset.
+        """
+        self.bus.write_byte_data(self.address, self.PWR_MGMT_1, 0x01)
+
+    def get_sleep_enabled(self):
+        """
+        Gets sleep mode status 
+        """
+        return self.read_i2c_bit(self.PWR_MGMT_1, self.PWR_1_SLEEP_BIT)
+
+    def set_sleep_enabled(self, enabled):
+        """
+        Sets the sleep mode status
+        """
+        self.write_i2c_bit(self.address, self.PWR_MGMT_1, self.PWR1_SLEEP_BIT, enabled)
+
+    def get_wake_cycle_enabled(self):
+        """
+        Gets the wake cycle enabled status
+        """
+        return self.read_i2c_bit(self.PWR_MGMT_1, self.PWR_1_CYCLE_BIT)
+
+    def set_wake_cycle_enabled(self, enabled):
+        """
+        Sets the wake cycle enabled status
+        """
+        self.write_i2c_bit(self.address, self.PWR_MGMT_1, self.PWR_1_CYCLE_BIT, enabled)
+
+
+    def get_temp_sensor_enabled(self):
+        """
+        Get temperature sensor enabled status.
+        Note: This register stores the disabled value, but for consistency 
+        with the rest of the code, the function is named and used with 
+        standard true/false values to indicate whether the sensor is 
+        enabled or disabled, respectively.
+        """
+        return (self.read_i2c_bit(self.PWR_MGMT_1, self.PWR1_TEMP_DIS_BIT)) == 0
+        
+
+    def set_temp_sensor_enabled(self, enabled):
+        """
+        Set temperature sensor enabled status.
+        Note: This register stores the disabled value, but for consistency 
+        with the rest of the code, the function is named and used with 
+        standard true/false values to indicate whether the sensor is 
+        enabled or disabled, respectively.
+        """
+        self.write_i2c_bit(self.address, self.PWR_MGMT_1, self.PWR_1_TEMP_DIS_BIT, ~enabled)
+
+    def get_clock_source(self):
+        """
+        Get clock source setting
+        """
+        return self.read_i2c_bits(self.address, self.PWR_MGMT_1, self.PWR1_CLKSEL_BIT, self.PWR1_CLKSEL_LENGTH)
+
+    
+        
+
 
 if __name__ == "__main__":
     mpu = mpu6050(0x68)
